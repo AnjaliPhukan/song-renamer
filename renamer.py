02938d20739d6c304a23414d6e820e6b07b69997
Anjali Phukan <zara.phukan>
2025-12-16 12:26:34 -0400

    initialized uv project, added tinytag as dependency to read file metadata

---
+++ renamer.py
@@ -0,0 +1,14 @@
+# Python3 program to rename files using their metadata
+# By default, renames it to "[Title] - [Artist]"
+# Author: Anjali Phukan (https://github.com/AnjaliPhukan)
+# Creation Date: December 16, 2025
+
+import os
+from tinytag import TinyTag
+
+if __name__ == "__main__":
+    main()
+
+def main():
+    print("Hello")   
+
